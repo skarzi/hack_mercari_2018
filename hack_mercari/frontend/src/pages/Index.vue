a9488dcc35096c74c0e3f035368efa6ec2923033
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2018-10-20 14:46:03 +0200

    add index page with menu

--- hack_mercari/frontend/src/pages/Index.vue
+++ hack_mercari/frontend/src/pages/Index.vue
@@ -1,14 +1,97 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="flex">
+    <div class="menu row justify-cener text-center">
+      <div class="menu--logo col-xs-12 text-center text-justify">
+        <div class="row">
+          <div class="col-xs-12 text-center">
+            <img
+              class="logo"
+              src="~assets/quasar-logo-full.svg"
+              alt="Logo"
+            >
+          </div>
+          <div class="col-xs-12">
+            <p class="app-name text-center">
+              Our Name
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="menu--title col-xs-12 text-center">
+        Select your role
+      </div>
+      <div class="menu--options col-xs-12">
+        <div class="menu--option col-xs-12">
+          <q-btn
+            class="full-width"
+            color="primary"
+            label="Courier"
+            size="lg"
+            to=""
+          />
+        </div>
+        <div class="menu--option col-xs-12">
+          <q-btn
+            class="full-width"
+            color="primary"
+            label="Sender"
+            size="lg"
+            to=""
+          />
+        </div>
+        <div class="menu--option col-xs-12">
+          <q-btn
+            class="full-width"
+            color="primary"
+            size="lg"
+            label="Recipient"
+            to=""
+          />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<style>
-</style>
-
 <script>
+import { createNamespacedHelpers } from 'vuex'
+
+const { mapActions } = createNamespacedHelpers('conditions')
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods: {
+    ...mapActions(['setToolbarVisibility'])
+  },
+  mounted () {
+    this.setToolbarVisibility(false)
+  }
 }
 </script>
+
+<style scope lang="stylus">
+.menu
+  margin 80px 20px
+
+.menu--logo
+  margin-top 20px
+
+.logo
+  height 70px
+
+.app-name
+  font-size 2.5em
+  font-weight bold
+  letter-spacing 1.4px
+  margin-top 20px
+
+.menu--title
+  margin-top 50px
+  font-size 3em
+  font-weight 300
+  line-height normal
+  letter-spacing 1.8px
+
+.menu--option
+  margin 32px 0px
+</style>
